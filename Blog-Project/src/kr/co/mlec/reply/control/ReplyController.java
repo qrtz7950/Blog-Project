c44ba1404599c2d0fdb2ca671cbfd9805c026971
Ddock2 <bmkwak22@gmail.com>
2018-08-30 21:55:29 +0900

    댓글 끝, 글 조회수 끝,

--- Blog-Project/src/kr/co/mlec/reply/control/ReplyController.java
+++ Blog-Project/src/kr/co/mlec/reply/control/ReplyController.java
@@ -9,21 +9,28 @@ import kr.co.mlec.framework.ModelAndView;
 import kr.co.mlec.framework.annotation.Controller;
 import kr.co.mlec.framework.annotation.RequestMapping;
 import kr.co.mlec.reply.service.ReplyService;
+import kr.co.mlec.reply.vo.ReplyVO;
 
 @Controller
-public class ReplyController extends HttpServlet{
+public class ReplyController extends HttpServlet {
 	
-	@RequestMapping("reply/write.do")
+	@RequestMapping("/reply/write.do")
 	public ModelAndView write(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ReplyVO reply = new ReplyVO();
+		reply.setBoard_no(Integer.parseInt(request.getParameter("board_no")));
+		reply.setId(request.getParameter("id"));
+		reply.setContent(request.getParameter("content"));
+		reply.setDepth(Integer.parseInt(request.getParameter("depth")));
+		reply.setSeq(Integer.parseInt(request.getParameter("seq")));
 		
 		ServletContext sc = request.getServletContext();
 		ReplyService service =  (ReplyService) sc.getAttribute("replyService");
 		
 		
-		service.write();
+		service.write(reply);
 		
 		ModelAndView mav = new ModelAndView();
-		mav.setView("/jsp/blog/replyWriteProcess.jsp");
+		mav.setView("/blog/detailBoard.do?board_no="+reply.getBoard_no());
 		
 		return mav;
 	}
